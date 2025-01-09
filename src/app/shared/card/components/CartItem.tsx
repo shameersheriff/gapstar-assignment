84448349fc19f870fef9734fbb196c76837e7b14
Shameer Sheriff <shameersheriff100@gmail.com>
2025-01-09 13:27:21 +0530

    Update card components

--- src/app/shared/card/components/CartItem.tsx
+++ src/app/shared/card/components/CartItem.tsx
@@ -9,11 +9,13 @@ const CartItem = ({
 }) => {
   return (
     <div className="w-full flex flex-row justify-between">
-      <div className="flex flex-row">
-        <div className="icon"><img src={icon} /></div>
-        <div className="title">{title}</div>
+      <div className="flex flex-row items-center gap-[8px]">
+        <div className="h-[24px] w-[24px] rounded-[2px]">
+          <img src={icon} />
+        </div>
+        <div className="text-[14px] font-[400]">{title}</div>
       </div>
-      <div className="jobs">{jobs}</div>
+      <div className="text-[14px] font-[400] text-[#64748B]">{jobs}</div>
     </div>
   );
 };
